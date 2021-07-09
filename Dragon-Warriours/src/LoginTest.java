5ff115531497e40cc4c030433bc007d513fb8d8d
aizhamalkenenbaeva <aikosha1954@gmail.com>
2021-07-09 00:28:45 -0400

    test

---
+++ Dragon-Warriours/src/LoginTest.java
@@ -0,0 +1,7 @@
+public class LoginTest {
+    public static void main(String[] args) {
+        String username = "userbond";
+        String password = "1234pass";
+        System.out.println("Login is successful");
+    }
+}
