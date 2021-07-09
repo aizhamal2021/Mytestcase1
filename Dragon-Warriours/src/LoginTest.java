e0cf820228917f7ec6301eb8ab909d3276c869d7
aizhamalkenenbaeva <aikosha1954@gmail.com>
2021-07-09 00:43:34 -0400

    fix the bugs

--- Dragon-Warriours/src/LoginTest.java
+++ Dragon-Warriours/src/LoginTest.java
@@ -2,6 +2,6 @@ public class LoginTest {
     public static void main(String[] args) {
         String username = "bondjames";
         String password = "12345pass";
-        System.out.println("Login is successful");
+
     }
 }
